9181402493d9282e96be498b218ec7d3e7fd253d
Minseo Lee <90029564+LMS10@users.noreply.github.com>
2025-02-03 22:38:55 +0900

    [#1] Feat/공통컴포넌트 버튼 (#6)
    * ✨ feat : 공통 버튼 컴포넌트 구현
    
    * 🐛 fix : <Link> <button> 역할 중복 문제 해결

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Link from 'next/link';
+
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset';
+  onClick?: () => void;
+  href?: string;
+  text: string;
+  disabled?: boolean;
+  className?: string;
+  variant?: 'primary' | 'oauth' | 'lightPurple';
+}
+
+export default function Button({ type = 'button', onClick, href, text, disabled = false, className = '', variant = 'primary' }: ButtonProps) {
+  const variantStyles =
+    variant === 'primary'
+      ? 'bg-purple-100 font-bold text-white'
+      : variant === 'oauth'
+        ? 'rounded-2xl border border-gray-300 bg-white px-[120px] py-[14px] font-medium text-gray-800'
+        : 'rounded-xl bg-purple-10 px-[36px] py-[16px] text-lg font-bold text-purple-100';
+
+  const finalClassName = `${variantStyles} ${className}`;
+
+  return href ? (
+    <Link href={href} className={finalClassName}>
+      {text}
+    </Link>
+  ) : (
+    <button type={type} onClick={onClick} disabled={disabled} className={finalClassName}>
+      {text}
+    </button>
+  );
+}
