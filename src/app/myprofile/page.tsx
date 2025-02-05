00b86cff5aa4f7e8c9101199a2e9bdd837c411b0
김은경 <kng4316@gmail.com>
2025-02-05 22:36:11 +0900

    💄 design : 공통컴포넌트 와인카드 css 반응형 작업

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -1,7 +1,12 @@
+import WineCard from '@/components/WineCard';
+
 export default function Page() {
   return (
-    <div>
+    <div className='flex flex-col gap-[60px]'>
       <div>프로필 페이지</div>
+      <WineCard />
+      <WineCard />
+      <WineCard />
     </div>
   );
 }
