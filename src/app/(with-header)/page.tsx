c51a32656b8511489e84490ca2363cbabbe2a91e
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2025-02-06 12:44:56 +0900

    [#10] Feat/랜딩페이지 (#17)
    * ✨ feat : 랜딩 페이지 UI
    
    * ✨ feat : 랜딩 페이지 작업 완료
    
    * 💄 design : 섹션 이미지 가운데 정렬 수정
    
    * 🔧 chore : Open Graph 이미지 해석을 위한 metadataBase 설정 추가
    
    * 💄 design : 부드럽고 자연스러운 반응형 구현

--- src/app/(with-header)/page.tsx
+++ src/app/(with-header)/page.tsx
@@ -1,3 +1,72 @@
+import Image from 'next/image';
+import { Metadata } from 'next';
+import Button from '@/components/Button';
+import bannerDesktop from '@/assets/images/banner_desktop.png';
+import bannerTablet from '@/assets/images/banner_tablet.png';
+import bannerMobile from '@/assets/images/banner_mobile.png';
+import section1Large from '@/assets/images/section1_lg.png';
+import section1Small from '@/assets/images/section1_sm.png';
+import section2Large from '@/assets/images/section2_lg.png';
+import section2Small from '@/assets/images/section2_sm.png';
+import section3Large from '@/assets/images/section3_lg.png';
+import section3Small from '@/assets/images/section3_sm.png';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://wine-lab.vercel.app/'),
+  title: 'WINE - 나만의 와인 창고',
+  description: '한 곳에서 관리하는 나만의 와인 창고',
+  openGraph: {
+    title: 'WINE - 나만의 와인 창고',
+    description: '한 곳에서 관리하는 나만의 와인 창고',
+    images: ['/thumbnail.png'],
+  },
+};
+
 export default function Home() {
-  return <div>홈페이지입니다.</div>;
+  return (
+    <div>
+      <div className='mx-auto flex min-h-[100vh] w-full max-w-[1200px] flex-col pb-[100px] pt-[94px] mobile:pt-[74px]'>
+        <div className='flex justify-center pt-[80px] tablet:pt-[24px]'>
+          <div className='relative h-[535px] w-[1140px] pc:block tablet:hidden mobile:hidden'>
+            <Image src={bannerDesktop} alt='데스크톱 배너 이미지' style={{ objectFit: 'cover' }} priority />
+          </div>
+          <div className='relative mx-6 h-auto w-auto min-w-[704px] pc:hidden tablet:block tablet:max-w-[1000px] mobile:hidden'>
+            <Image src={bannerTablet} alt='태블릿 배너 이미지' style={{ objectFit: 'cover' }} />
+          </div>
+          <div className='relative mx-5 h-auto w-auto min-w-[343px] max-w-[700px] pc:hidden tablet:hidden mobile:block'>
+            <Image src={bannerMobile} alt='모바일 배너 이미지' style={{ objectFit: 'cover' }} />
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-[96px] pt-[160px] tablet:pt-[80px] mobile:gap-[48px] mobile:pt-[48px]'>
+          <div className='flex max-w-[699px] flex-col items-center pl-[25px] tablet:max-w-[800px] tablet:pl-[32px] mobile:pl-[0]'>
+            <div className='relative h-[320px] w-[699px] pc:block tablet:block mobile:hidden'>
+              <Image src={section1Large} alt='데스크톱, 태블릿 섹션 이미지1' style={{ objectFit: 'cover' }} />
+            </div>
+            <div className='relative h-auto w-auto pc:hidden tablet:hidden mobile:block mobile:min-w-[343px] mobile:max-w-[550px]'>
+              <Image src={section1Small} alt='모바일 섹션 이미지1' style={{ objectFit: 'cover' }} />
+            </div>
+          </div>
+          <div className='flex max-w-[665px] flex-col items-center'>
+            <div className='relative h-[320px] w-[665px] pc:block tablet:block mobile:hidden'>
+              <Image src={section2Large} alt='데스크톱, 태블릿 섹션 이미지2' style={{ objectFit: 'cover' }} />
+            </div>
+            <div className='relative h-auto w-auto pc:hidden tablet:hidden mobile:block mobile:min-w-[343px] mobile:max-w-[550px]'>
+              <Image src={section2Small} alt='모바일 섹션 이미지2' style={{ objectFit: 'cover' }} />
+            </div>
+          </div>
+          <div className='flex max-w-[665px] flex-col items-center'>
+            <div className='relative h-[320px] w-[665px] pc:block tablet:block mobile:hidden'>
+              <Image src={section3Large} alt='데스크톱, 태블릿 섹션 이미지3' style={{ objectFit: 'cover' }} />
+            </div>
+            <div className='relative h-auto w-auto pc:hidden tablet:hidden mobile:block mobile:min-w-[343px] mobile:max-w-[550px]'>
+              <Image src={section3Small} alt='모바일 섹션 이미지3' style={{ objectFit: 'cover' }} />
+            </div>
+          </div>
+        </div>
+        <div className='mx-auto flex items-center justify-center pt-[104px] tablet:pt-[80px] mobile:pt-[64px]'>
+          <Button text='와인 보러가기' href='/wines' className='rounded-[100px] px-[96px] py-[16px] text-lg' />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,10 @@ import profileDefault from '@/assets/icons/profile_default.svg';
 
 function LoggedOutHeader() {
   return (
-    <div className='mt-[24px] flex w-full justify-center'>
-      <div className='box-border h-[70px] w-[1140px] rounded-xl bg-black tablet:w-[740px] mobile:h-[50px] mobile:w-[343px]'>
+    <div className='fixed top-[24px] z-50 flex w-full justify-center'>
+      <div className='mx-auto h-[70px] w-[1140px] rounded-xl bg-black tablet:mx-6 tablet:max-w-[1000px] mobile:mx-5 mobile:h-[50px] mobile:min-w-[343px] mobile:max-w-[700px]'>
         <header className='flex items-center justify-between px-[60px] py-[22.5px] mobile:px-[20px] mobile:py-[17.5px]'>
-          <Link href={'/'}>
+          <Link href='/'>
             <Image className='cursor-pointer' src={logoWhite} width={52} height={15} alt='화이트 로고' />
           </Link>
           <div className='flex cursor-pointer gap-[40px] text-base text-white mobile:gap-[20px] mobile:text-md mobile:leading-none'>
@@ -28,10 +28,10 @@ function LoggedInHeader() {
   const { profileImage } = useAuth();
 
   return (
-    <div className='mt-[24px] flex w-full justify-center'>
-      <div className='box-border h-[70px] w-[1140px] rounded-xl bg-black tablet:w-[740px] mobile:h-[50px] mobile:w-[343px]'>
+    <div className='fixed top-[24px] z-50 flex w-full justify-center'>
+      <div className='mx-auto h-[70px] w-[1140px] rounded-xl bg-black tablet:mx-6 tablet:max-w-[1000px] mobile:mx-5 mobile:h-[50px] mobile:min-w-[343px] mobile:max-w-[700px]'>
         <header className='flex items-center justify-between px-[60px] py-[12.5px] mobile:px-[20px] mobile:py-[12.5px]'>
-          <Link href={'/'}>
+          <Link href='/'>
             <Image src={logoWhite} width={52} height={15} alt='화이트 로고' />
           </Link>
           <ProfileImg size='small' src={profileImage || profileDefault} isLine onClick={() => {}} />
