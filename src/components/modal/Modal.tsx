3d7569cc430498ed1ee6dee27335e39bf971def4
park521 <gh10035@naver.com>
2025-02-07 11:24:05 +0900

    :art: chore : esc 와 영역 밖 클릭시 상호작용을 모달 내부로 변경

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,17 +1,46 @@
 'use client';
 
-import { ReactNode, useEffect, useRef, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 interface ModalProps {
   children: ReactNode;
   isOpen: boolean;
+  setIsOpen: (setOpen: boolean) => void;
   className?: string;
 }
 
-export default function Modal({ children, isOpen, className }: ModalProps) {
+export default function Modal({ children, isOpen, setIsOpen, className }: ModalProps) {
   const dialogRef = useRef<HTMLDialogElement>(null);
   const [mounted, setMounted] = useState(false);
+
+  const escKeyModalClose = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setIsOpen(false);
+      }
+    },
+    [setIsOpen],
+  );
+
+  const handleClickOutside = useCallback(
+    (e: MouseEvent) => {
+      if ((e.target as HTMLElement).nodeName === 'DIALOG') {
+        setIsOpen(false);
+      }
+    },
+    [setIsOpen],
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', escKeyModalClose);
+    return () => window.removeEventListener('keydown', escKeyModalClose);
+  }, [escKeyModalClose]);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [handleClickOutside]);
 
   useEffect(() => {
     setMounted(true);
