5c7a108e9ba1289879e5f9f86b0752f0b9b098d5
qpalkim <ghs0199@naver.com>
2025-02-08 13:17:57 +0900

    ✨ feat : 리뷰 모달 컨트롤바 컴포넌트 구현

--- src/app/(with-header)/wines/[id]/_components/Modal.tsx
+++
@@ -1,9 +0,0 @@
-import Taste from '@/components/Taste';
-
-export default function Modal() {
-  return (
-    <div className='modal'>
-      <Taste lightBold={4} smoothTannic={2} drySweet={3} softAcidic={1} isDraggable={true} />
-    </div>
-  );
-}
--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -2,17 +2,24 @@
 import { useEffect, useRef, useState } from 'react';
 
 type ControlBarProps = {
-  label?: string;
-  minLabel?: string;
-  maxLabel?: string;
+  label: string;
+  minLabel: string;
+  maxLabel: string;
   value: number;
-  onChange: (newValue: number) => void;
   isDraggable: boolean;
-  minValue?: number;
-  maxValue?: number;
+  size: 'small' | 'medium' | 'large';
+  name: string;
+  onChange: (name: string, value: number) => void;
 };
 
-export default function ControlBar({ label, minLabel, maxLabel, value, onChange, isDraggable }: ControlBarProps) {
+export default function ControlBar({ label, minLabel, maxLabel, value, isDraggable, size = 'large', onChange, name }: ControlBarProps) {
+  const controlBarStyle =
+    size === 'large'
+      ? 'max-w-[720px] h-[28px] tablet:max-w-[624px] tablet:h-[26px]  mobile:max-w-[303px] mobile:h-[30px]'
+      : size === 'medium'
+        ? 'max-w-[540px] h-[25px] tablet:max-w-[704px]  mobile:max-w-[343px] mobile:h-[24px]'
+        : 'max-w-[480px] h-[26px] mobile:max-w-[327px] mobile:h-[24px]';
+
   const [dragging, setDragging] = useState(false);
   const [dragValue, setDragValue] = useState(value);
   const controlBarRef = useRef<HTMLDivElement | null>(null);
@@ -42,7 +49,7 @@ export default function ControlBar({ label, minLabel, maxLabel, value, onChange,
       let newValue = initialValue.current + (deltaX / controlBarWidth) * 10;
       newValue = Math.min(Math.max(Math.round(newValue), 1), 10);
       setDragValue(newValue);
-      onChange(newValue);
+      onChange(name, newValue);
     };
 
     const handleMouseUp = () => {
@@ -58,23 +65,23 @@ export default function ControlBar({ label, minLabel, maxLabel, value, onChange,
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('mouseup', handleMouseUp);
     };
-  }, [dragging, dragValue, onChange]);
+  }, [dragging, dragValue, onChange, name]);
 
   // const formattedValue = typeof dragValue === 'number' && !isNaN(dragValue) ? dragValue.toFixed(1) : '0';
 
   return (
-    <div className='mb-[19px] w-full'>
-      <div className='mb-2 flex items-center justify-between'>
-        {label && <label className='mr-[16px] h-[28px] w-[56px] flex-none rounded-[6px] bg-gray-100 text-center text-md font-semibold leading-[28px] text-gray-500'>{label}</label>}
-        {minLabel && <span className='mr-[16px] w-[70px] flex-none whitespace-nowrap text-lg font-medium text-gray-800'>{minLabel}</span>}
+    <div className={`mb-[18px] mobile:mb-[12px] ${controlBarStyle}`}>
+      <div className='flex items-center justify-between'>
+        <label className='mr-[16px] h-[28px] w-[56px] flex-none rounded-[6px] bg-gray-100 text-center text-md font-semibold leading-[28px] text-gray-500'>{label}</label>
+        <span className='mr-[16px] w-[70px] flex-none whitespace-nowrap text-lg font-medium text-gray-800'>{minLabel}</span>
         <div className='relative h-[6px] w-full cursor-pointer rounded-full border-[1px] border-solid border-gray-300 bg-gray-100' ref={controlBarRef} onMouseDown={handleMouseDown}>
           <div
-            className='absolute left-0 top-1/2 h-[16px] w-[16px] -translate-y-1/2 transform cursor-pointer rounded-full bg-purple-100'
+            className='absolute left-0 top-1/2 h-[16px] w-[16px] -translate-y-1/2 transform cursor-pointer rounded-full bg-purple-100 mobile:h-[12px] mobile:w-[12px]'
             style={{ left: `${(dragValue / 10) * 100}%` }}
             onMouseDown={handleMouseDown}
           />
         </div>
-        {maxLabel && <span className='ml-[16px] w-[70px] flex-none whitespace-nowrap text-right text-lg font-medium text-gray-800'>{maxLabel}</span>}
+        <span className='ml-[16px] w-[70px] flex-none whitespace-nowrap text-right text-lg font-medium text-gray-800'>{maxLabel}</span>
       </div>
     </div>
   );
