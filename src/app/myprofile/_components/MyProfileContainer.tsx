699c55e12b58c9310428e893b8c0188ede5b3545
김은경 <kng4316@gmail.com>
2025-02-09 16:18:07 +0900

    ♻️ refactor : MyProfile 코드 정리

--- src/app/myprofile/_components/MyProfileContainer.tsx
+++ src/app/myprofile/_components/MyProfileContainer.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import ProfileImg from '@/components/ProfileImg';
-import Button from '@/components/Button';
-import { fetchWithAuth } from '@/lib/auth';
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import photoIcon from '@/assets/icons/photo.svg';
+import ProfileImg from '@/components/ProfileImg';
+import Button from '@/components/Button';
+import { fetchWithAuth } from '@/lib/auth';
 
 interface MyProfileData {
   id: number;
@@ -16,23 +16,21 @@ interface MyProfileData {
   updatedAt: string;
 }
 
-export function MyProfile({
-  profileData,
-  upLoadImgFile,
-  upLoadUserData,
-}: {
+interface MuProfileProps {
   profileData: MyProfileData;
-  upLoadImgFile: (formdata: FormData) => Promise<string | undefined>;
+  upLoadImgFile: (formData: FormData) => Promise<string | undefined>;
   upLoadUserData: (image: string, nickname: string) => Promise<string | undefined>;
-}) {
-  const [nickNameValue, setNickNameValue] = useState(profileData.nickname);
-  const [isButtonDisable, setIsButtonDisable] = useState(true);
-  const [previewImg, setPreviewImg] = useState(profileData.image);
-  const [file, setFile] = useState<File | null>(null);
-  const fileInputRef = useRef(null);
+}
 
+export function MyProfile({ profileData, upLoadImgFile, upLoadUserData }: MuProfileProps) {
+  const [isDisableProfile, setIsDisableProfile] = useState(true);
+  const [nickNameValue, setNickNameValue] = useState(profileData.nickname);
+  const [preNickName, setPreNickName] = useState(profileData.nickname);
+  const [profileImg, setProfileImg] = useState(profileData.image);
+  const [fileInput, setFileInput] = useState<File | null>(null);
+  const fileInputRef = useRef(null);
   const nickNameRef = useRef(nickNameValue);
-  const previewImgRef = useRef(previewImg);
+  const profileImgRef = useRef(profileImg);
 
   const handleNickNameInput = (e: ChangeEvent<HTMLInputElement>) => {
     setNickNameValue(e.target.value);
@@ -40,59 +38,55 @@ export function MyProfile({
   };
 
   const onClickEditButton = () => {
-    setIsButtonDisable(false);
+    setIsDisableProfile(false);
   };
 
   const onClickUploadButton = async () => {
-    if (!file) {
-      setIsButtonDisable(true);
+    if (!fileInput && nickNameValue === preNickName) {
+      setIsDisableProfile(true);
       return;
+    } else if (fileInput) {
+      const formData = new FormData();
+      formData.append('image', fileInput);
+
+      const imgResult = await upLoadImgFile(formData);
+      if (!imgResult) return;
+      setProfileImg(imgResult);
+      profileImgRef.current = imgResult;
     }
-
-    const formData = new FormData();
-    formData.append('image', file);
-
-    const imgResult = await upLoadImgFile(formData);
-    if (!imgResult) return;
-    setPreviewImg(imgResult);
-    previewImgRef.current = imgResult;
-
-    const UserResult = await upLoadUserData(previewImgRef.current, nickNameRef.current);
+    const UserResult = await upLoadUserData(profileImgRef.current, nickNameRef.current);
     if (!UserResult) return;
-    URL.createObjectURL(file);
-    setFile(null);
-    setIsButtonDisable(true);
+    setPreNickName(nickNameRef.current);
+    URL.revokeObjectURL(profileImg);
+    setFileInput(null);
+    setIsDisableProfile(true);
   };
 
   const handleChangeFileInput = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) {
       return;
     }
-
     const value = e.target.files[0];
-    setFile(value);
+    setFileInput(value);
     const nextImg = URL.createObjectURL(value);
-    setPreviewImg(nextImg);
+    setProfileImg(nextImg);
   };
-
-  useEffect(() => {}, []);
 
   return (
     <div className='flex h-[530px] w-[280px] flex-col items-center justify-between rounded-[16px] border border-gray-300 px-[20px] py-[39px] shadow-drop tablet:h-[247px] tablet:w-full tablet:px-[40px] tablet:py-[23px] mobile:h-[241px] mobile:p-[20px]'>
       <div className='flex w-full flex-col items-center justify-center gap-[32px] tablet:flex-row tablet:justify-start mobile:gap-[16px]'>
-        {isButtonDisable ? (
-          <ProfileImg src={previewImg} size='large' />
+        {isDisableProfile ? (
+          <ProfileImg src={profileImg} size='large' />
         ) : (
           <label htmlFor='profileImg' className='relative h-[164px] w-[164px] cursor-pointer rounded-full tablet:h-[80px] tablet:w-[80px] mobile:h-[60px] mobile:w-[60px]'>
-            <ProfileImg src={previewImg} size='large' />
+            <ProfileImg src={profileImg} size='large' />
             <div className='absolute left-1/2 top-1/2 flex h-[164px] w-[164px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-purple-100 opacity-30 tablet:h-[80px] tablet:w-[80px] mobile:h-[60px] mobile:w-[60px]'></div>
             <Image className='absolute left-1/2 top-1/2 z-10 h-[40px] w-[40px] -translate-x-1/2 -translate-y-1/2' src={photoIcon} alt='사진 로고' />
             <input id='profileImg' type='file' hidden accept='image/png, image/jpeg' ref={fileInputRef} onChange={handleChangeFileInput} />
           </label>
         )}
         <div className='flex flex-col gap-[16px] tablet:gap-[8px] mobile:gap-[4px]'>
-          <h1 className='flex justify-center text-2xl font-bold text-gray-800 tablet:justify-start mobile:text-xl'>{nickNameValue}</h1>
-          {/* <p className='flex justify-center text-lg font-normal text-gray-500 tablet:justify-start mobile:text-md'>wanda@email.com</p> */}
+          <h1 className='flex justify-center text-2xl font-bold text-gray-800 tablet:justify-start mobile:text-xl'>{preNickName}</h1>
         </div>
       </div>
       <div className='flex w-full flex-col justify-center gap-[8px] tablet:flex-row tablet:gap-[24px] mobile:flex-col mobile:gap-[6px]'>
@@ -103,14 +97,14 @@ export function MyProfile({
           <input
             id='nickname'
             type='text'
-            disabled={isButtonDisable}
-            className={`flex h-[48px] w-full rounded-2xl border border-gray-300 pl-5 text-lg font-normal focus:outline-purple-100 mobile:h-[42px] mobile:text-md ${isButtonDisable ? `text-gray-500` : `text-gray-800`}`}
+            disabled={isDisableProfile}
+            className={`flex h-[48px] w-full rounded-2xl border border-gray-300 pl-5 text-lg font-normal focus:outline-purple-100 mobile:h-[42px] mobile:text-md ${isDisableProfile ? `text-gray-500` : `text-gray-800`}`}
             onChange={handleNickNameInput}
             value={nickNameValue}
           />
         </div>
         <div className='flex flex-row-reverse tablet:items-end'>
-          {isButtonDisable ? (
+          {isDisableProfile ? (
             <Button
               text='변경하기'
               onClick={onClickEditButton}
@@ -135,9 +129,11 @@ export default function MyProfileContainer() {
   const getUserData = async () => {
     try {
       const response = await fetchWithAuth(`${process.env.NEXT_PUBLIC_BASE_URL}/users/me`);
+
       if (!response?.ok || response === null) {
         return;
       }
+
       const data: MyProfileData = await response.json();
       setProfileData(data);
     } catch (error) {
@@ -152,8 +148,9 @@ export default function MyProfileContainer() {
         body: formData,
       });
 
-      if (!response) throw new Error('API 요청 실패: 응답이 없습니다.');
-      if (!response.ok) throw new Error('업로드 실패');
+      if (!response?.ok || response === null) {
+        return;
+      }
 
       const data = await response.json();
       return data.url as string;
@@ -173,8 +170,9 @@ export default function MyProfileContainer() {
         body: JSON.stringify({ image: image, nickname: nickname }),
       });
 
-      if (!response) throw new Error('API 요청 실패: 응답이 없습니다.');
-      if (!response.ok) throw new Error('프로필 적용 실패');
+      if (!response?.ok || response === null) {
+        return;
+      }
 
       const result = await response.json();
       return result;
@@ -188,7 +186,7 @@ export default function MyProfileContainer() {
     getUserData();
   }, []);
 
-  if (!profileData) return;
+  if (!profileData) return <div>로딩중...</div>;
 
   return <MyProfile profileData={profileData} upLoadImgFile={upLoadImgFile} upLoadUserData={upLoadUserData} />;
 }
--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -1,10 +1,7 @@
-import MyProfile from './_components/MyProfileContainer';
-
 export default function Page() {
   return (
     <div>
       <div>프로필 페이지</div>
-      <MyProfile />
     </div>
   );
 }
