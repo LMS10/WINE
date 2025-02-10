596b8e8888403e790e90422511ae0885d6dc0783
김은경 <kng4316@gmail.com>
2025-02-10 23:15:19 +0900

    ✨ feat : MyReviewListContainer 데이터 연결

--- src/app/myprofile/_components/MyReviewList.tsx
+++ src/app/myprofile/_components/MyReviewList.tsx
@@ -1,17 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 import elapsedTime from '@/utils/formatDate';
 import like from '@/assets/icons/star_hover.svg';
-import kebab from '@/assets/icons/dot.svg';
-import myReviewMockData from '@/app/myprofile/_components/myReviewMockData.json';
+import { useEffect, useState } from 'react';
+import { fetchWithAuth } from '@/lib/auth';
+import emptyData from '@/assets/icons/empty_review.svg';
+import { MyReview, MyReviewResponse } from '@/types/review-data';
+import KebabDropDown from './KebabDropDown';
 
 interface MyReviewProps {
+  id: number;
   rating: number;
   createdAt: string;
   wineName: string;
   content: string;
 }
 
-export function MyReviewItem({ rating, createdAt, wineName, content }: MyReviewProps) {
+export function MyReviewItem({ id, rating, createdAt, wineName, content }: MyReviewProps) {
   const reviewElapsedTime = elapsedTime(createdAt);
 
   return (
@@ -20,9 +26,9 @@ export function MyReviewItem({ rating, createdAt, wineName, content }: MyReviewP
         <div className='relative flex gap-[15px]'>
           <div className='flex h-[42px] w-[80px] items-center justify-center gap-[4px] rounded-[12px] bg-purple-10 tablet:h-[38px] mobile:h-[32px] mobile:w-[60px]'>
             <Image className='w-[20px] mobile:w-[16px]' src={like} alt='별점 아이콘' />
-            <span className='text-[18px] font-bold text-purple-100 mobile:text-[14px]'>{rating}</span>
+            <span className='text-[18px] font-bold text-purple-100 mobile:text-[14px]'>{rating.toFixed(1)}</span>
           </div>
-          <Image className='absolute right-0 w-[26px] cursor-pointer mobile:w-[24px]' src={kebab} alt='케밥 아이콘' />
+          <KebabDropDown id={id} />
           <span className='flex items-center justify-center text-[16px] text-gray-500 mobile:text-[14px]'>{reviewElapsedTime}</span>
         </div>
         <div className='flex flex-col gap-[10px]'>
@@ -34,11 +40,46 @@ export function MyReviewItem({ rating, createdAt, wineName, content }: MyReviewP
   );
 }
 
-export default function MyReviewList() {
+export default function MyReviewListContainer() {
+  const [myReviewData, setMyReviewData] = useState<MyReview[]>([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  const getMyReview = async () => {
+    try {
+      setIsloading(true);
+      const response = await fetchWithAuth(`${process.env.NEXT_PUBLIC_BASE_URL}/users/me/reviews?limit=5`);
+
+      if (!response?.ok || response === null) {
+        return;
+      }
+
+      const data: MyReviewResponse = await response.json();
+      setMyReviewData(data.list);
+    } catch (error) {
+      console.error('데이터를 불러오는데 오류가 발생했습니다:', error);
+    } finally {
+      setIsloading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMyReview();
+  }, []);
+
+  if (isLoading) return <div>로딩중...</div>;
+
+  if (myReviewData.length === 0)
+    return (
+      <div className='flex h-[80vh] max-w-[800px] flex-col items-center justify-center gap-[24px] mobile:h-[40vh] mobile:gap-[12px]'>
+        <Image className='h-[120px] w-[120px] mobile:h-[50px] mobile:w-[50px]' alt='데이터 없음' src={emptyData} priority />
+        <p className='text-lg font-normal text-gray-500 mobile:text-md'>내가 등록한 후기가 없어요</p>
+      </div>
+    );
+
   return (
     <div className='flex flex-col gap-[8px] tablet:gap-[16px] mobile:gap-[16px]'>
-      {myReviewMockData.list.map((value) => (
-        <MyReviewItem key={value.id} rating={value.rating} createdAt={value.createdAt} wineName={value.wine.name} content={value.content} />
+      {myReviewData.map((value) => (
+        <MyReviewItem key={value.id} rating={value.rating} createdAt={value.createdAt} wineName={value.wine.name} content={value.content} id={value.id} />
       ))}
     </div>
   );
