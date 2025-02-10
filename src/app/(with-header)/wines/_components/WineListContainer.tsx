246e2f4ef77f8163daab0249e6378fd3004b8b6f
LMS10 <ggb7410@naver.com>
2025-02-10 17:19:14 +0900

    ✨ feat : WineFilter 컴포넌트 구현

--- src/app/(with-header)/wines/_components/WineListContainer.tsx
+++ src/app/(with-header)/wines/_components/WineListContainer.tsx
@@ -1,28 +1,67 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Image from 'next/image';
 import { WineDetails } from '@/types/wine';
 import SearchBar from './SearchBar';
 import WineFilter from './WineFilter';
+import WineFilterModal from './WineFilterModal';
 import WineCard from './WineCard';
 import filterIcon from '@/assets/icons/filter.svg';
 
+const MAX_PRICE = 2000000;
+
 export default function WineListContainer() {
   const [wines, setWines] = useState<WineDetails[]>([]);
-  const [selectedType, setSelectedType] = useState<string | null>(null);
   const [searchQuery, setSearchQuery] = useState<string>('');
+  const [filters, setFilters] = useState<{ type: string | null; minPrice: number; maxPrice: number; rating: number | null }>({
+    type: null,
+    minPrice: 0,
+    maxPrice: MAX_PRICE,
+    rating: null,
+  });
   const [isFilterModalOpen, setFilterModalOpen] = useState<boolean>(false);
+  const [pendingFilters, setPendingFilters] = useState(filters);
+
+  useEffect(() => {
+    if (isFilterModalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isFilterModalOpen]);
+
+  const handleKeyDown = useCallback((event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setFilterModalOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  const applyFilters = () => {
+    setFilters(pendingFilters);
+    setFilterModalOpen(false);
+  };
 
   useEffect(() => {
     const fetchWines = async () => {
       const params = new URLSearchParams();
-      if (selectedType) params.append('type', selectedType.toUpperCase());
+      if (filters.type) params.append('type', filters.type.toUpperCase());
+      if (filters.minPrice) params.append('minPrice', filters.minPrice.toString());
+      if (filters.maxPrice) params.append('maxPrice', filters.maxPrice.toString());
+      if (filters.rating) params.append('rating', filters.rating.toString());
       if (searchQuery) params.append('name', searchQuery);
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/wines?limit=10&${params.toString()}`, {
-        cache: 'force-cache',
-      });
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/wines?limit=20&${params.toString()}`);
 
       if (!res.ok) {
         console.error('Failed to fetch wines');
@@ -34,43 +73,25 @@ export default function WineListContainer() {
     };
 
     fetchWines();
-  }, [selectedType, searchQuery]);
-
-  const handleFilterButtonClick = () => {
-    setFilterModalOpen(!isFilterModalOpen);
-  };
+  }, [filters, searchQuery]);
 
   return (
     <div className='mt-10 grid grid-cols-[284px,1fr] grid-rows-[48px,1fr] gap-x-[60px] gap-y-[62px] tablet:flex tablet:flex-col mobile:mt-5 mobile:gap-[30px]'>
       <div className='hidden pc:block' />
-
       <div className='hidden pc:block'>
         <SearchBar onSearch={setSearchQuery} />
       </div>
-
       <div className='hidden pc:block'>
-        <WineFilter onChangeType={setSelectedType} />
+        <WineFilter onChangeFilter={setFilters} />
       </div>
-
       <div className='flex w-full items-center justify-between pc:hidden'>
-        <button className='mr-6 flex h-12 w-12 items-center justify-center rounded-lg border border-gray-300 mobile:mr-4 mobile:h-[38px] mobile:w-[38px]' onClick={handleFilterButtonClick}>
-          <Image src={filterIcon} alt='와인 필터 아이콘' className='h-[26px] w-[26px] mobile:h-[22px] mobile:w-[22px]' />
+        <button className='mr-6 flex h-12 w-12 items-center justify-center rounded-lg border border-gray-300' onClick={() => setFilterModalOpen(true)}>
+          <Image src={filterIcon} alt='와인 필터 아이콘' width={26} height={26} />
         </button>
         <SearchBar onSearch={setSearchQuery} />
       </div>
-
-      {isFilterModalOpen && (
-        <div className='fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50'>
-          <div className='rounded-md bg-white p-6'>
-            <WineFilter onChangeType={setSelectedType} />
-            <button className='mt-4 rounded-full bg-red-500 px-4 py-2 text-white' onClick={() => setFilterModalOpen(false)}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-
-      <div className='flex flex-1 flex-col gap-[62px] mobile:gap-5'>
+      <WineFilterModal isOpen={isFilterModalOpen} onClose={() => setFilterModalOpen(false)} onApply={applyFilters} initialFilters={pendingFilters} onFilterChange={setPendingFilters} />
+      <div className='flex flex-1 flex-col gap-[62px]'>
         {wines.map((wine) => (
           <WineCard key={wine.id} wine={wine} />
         ))}
