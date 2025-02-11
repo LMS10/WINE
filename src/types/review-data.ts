08860a538104d8f1ea1f3c1d5fb7fda4b74653f0
김은경 <98223616+eunkyung01@users.noreply.github.com>
2025-02-11 14:12:22 +0900

    [#45] Feat/내프로필페이지 내가쓴후기리스트 (#51)
    * ✨ feat : MyReviewListContainer 데이터 연결
    
    * ♻️ refactor : MyReviewListContainer 코드 정리

--- src/types/review-data.ts
+++ src/types/review-data.ts
@@ -1,3 +1,5 @@
+import { Wine } from './wine';
+
 export interface ReviewData {
   avgRating: number;
   reviews: {
@@ -18,4 +20,29 @@ export interface ReviewData {
     };
     likes: boolean[];
   }[];
+}
+
+export interface MyReviewResponse {
+  list: MyReview[];
+  totalCount: number;
+  nextCursor: number | null;
+}
+
+export interface MyReview {
+  id: number;
+  rating: number;
+  lightBold: number;
+  smoothTannic: number;
+  drySweet: number;
+  softAcidic: number;
+  aroma: string[];
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  user: {
+    id: number;
+    nickname: string;
+    image: string;
+  };
+  wine: Wine;
 }
