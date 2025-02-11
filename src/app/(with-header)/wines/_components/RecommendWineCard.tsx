b6063edf4517317d959782b155ff4081c5af1c81
LMS10 <ggb7410@naver.com>
2025-02-11 17:00:42 +0900

    ✨ feat : RecommendWineCard 컴포넌트 구현

---
+++ src/app/(with-header)/wines/_components/RecommendWineCard.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import { Wine } from '@/types/wine';
+import StaticRating from '@/components/StaticRating';
+
+export default function RecommendWineCard({ image, name, avgRating }: Wine) {
+  return (
+    <div className='flex h-[185px] w-[232px] cursor-pointer justify-between gap-[28px] rounded-xl bg-white px-[30px] pt-6 mobile:h-[160px] mobile:w-[193px] mobile:gap-[25px] mobile:px-[25px]'>
+      <div className='relative h-[161px] w-[44px] overflow-hidden mobile:h-[136px] mobile:w-[38px]'>
+        <Image src={image} alt={name} sizes='10vw' fill className='absolute object-cover' />
+      </div>
+      <div className='flex flex-col gap-[5px] mobile:gap-0'>
+        <p className='text-4xl font-extrabold text-gray-800 mobile:text-[28px]'>{avgRating.toFixed(1)}</p>
+        <div className='mobile:hidden'>
+          <StaticRating size='medium' value={avgRating} />
+        </div>
+        <div className='pc:hidden tablet:hidden mobile:block'>
+          <StaticRating size='small' value={avgRating} />
+        </div>
+        <p className='w-[100px] text-[12px] leading-[18px] text-gray-500 mobile:w-[80px] mobile:text-[10px] mobile:leading-[14px]'>{name}</p>
+      </div>
+    </div>
+  );
+}
