a059a859da637af44c094793f7a06b76af8bb0e7
CJewon <112820744+CJewon@users.noreply.github.com>
2025-02-11 19:24:56 +0900

    :sparkles: feat : 카카오 로그인버튼 클릭 시 kakaoAuthUrl로 이동

---
+++ src/app/signin/api/KakaoApi.tsx
@@ -0,0 +1,25 @@
+export default async function KakaoApi() {
+  try {
+    const requestBody = {
+      appKey: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
+      provider: 'KAKAO',
+    };
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/oauthApps`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error(`인증 실패 : ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log('카카오 앱 등록 성공 :', data);
+    return data;
+  } catch (error) {
+    console.error('카카오 앱 등록 오류 :', error);
+    throw error;
+  }
+}
