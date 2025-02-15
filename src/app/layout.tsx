dac404daef3d0953b54b36dcdedd818005f18adb
LMS10 <ggb7410@naver.com>
2025-02-15 15:27:56 +0900

    💄 design : 토스트 css 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 import { AuthProvider } from '@/contexts/AuthProvider';
 import './globals.css';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, Slide } from 'react-toastify';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
@@ -21,7 +21,26 @@ export default function RootLayout({
           {children}
           <div id='modal-root'></div>
 
-          <ToastContainer toastStyle={{ minHeight: 'unset' }} autoClose={2000} closeButton={false} hideProgressBar position='bottom-center' />
+          <ToastContainer
+            toastStyle={{
+              minHeight: 'unset',
+              minWidth: 'unset',
+              marginBottom: '30px',
+            }}
+            toastClassName='flex w-fit items-center justify-center rounded-[12px] bg-gray-800 px-7 py-[10px] text-lg text-white mobile:text-md'
+            position='bottom-center'
+            autoClose={2000}
+            icon={false}
+            closeButton={false}
+            hideProgressBar
+            newestOnTop={false}
+            pauseOnFocusLoss
+            draggable
+            closeOnClick={true}
+            pauseOnHover
+            transition={Slide}
+            limit={3}
+          />
         </AuthProvider>
       </body>
     </html>
