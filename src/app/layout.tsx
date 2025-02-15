d7f110f1e710e76b93798f42d3b2cb70cc468a7f
Minseo Lee <90029564+LMS10@users.noreply.github.com>
2025-02-15 15:56:25 +0900

    [#83] Feat/공통컴포넌트 토스트 (#84)
    * ✨ feat : 레이아웃 토스트 적용
    
    * 💄 design : 토스트 css 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from 'next/font/local';
 import { AuthProvider } from '@/contexts/AuthProvider';
 import './globals.css';
+import { ToastContainer, Slide } from 'react-toastify';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
@@ -19,6 +20,27 @@ export default function RootLayout({
         <AuthProvider>
           {children}
           <div id='modal-root'></div>
+
+          <ToastContainer
+            toastStyle={{
+              minHeight: 'unset',
+              minWidth: 'unset',
+              marginBottom: '30px',
+            }}
+            toastClassName='flex w-fit items-center justify-center rounded-[12px] bg-gray-800 px-7 py-[10px] text-lg text-white mobile:text-md'
+            position='bottom-center'
+            autoClose={2000}
+            icon={false}
+            closeButton={false}
+            hideProgressBar
+            newestOnTop={false}
+            pauseOnFocusLoss
+            draggable
+            closeOnClick={true}
+            pauseOnHover
+            transition={Slide}
+            limit={3}
+          />
         </AuthProvider>
       </body>
     </html>
