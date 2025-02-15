9f37fc76d26a2d9ebec1cef5b66bf15c8562e472
LMS10 <ggb7410@naver.com>
2025-02-16 03:42:42 +0900

    ♻️ refactor : 추천 와인 목록 fetch 실패 시 Refresh 컴포넌트 추가하여 에러 처리 개선

--- src/app/(with-header)/wines/_components/RecommendWineContainer.tsx
+++ src/app/(with-header)/wines/_components/RecommendWineContainer.tsx
@@ -1,33 +1,45 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { fetchRecommendWine } from '@/lib/fetchRecommendWine';
+import { Wine } from '@/types/wine';
 import RecommendWine from './RecommendWine';
 import RecommendWineListSkeleton from './skeleton/RecommendWineListSkeleton';
-import { Wine } from '@/types/wine';
+import Refresh from '@/components/Refresh';
 
 export default function RecommendWineContainer() {
   const [recommendedList, setRecommendedList] = useState<Wine[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [isloading, setIsLoading] = useState<boolean>(true);
+  const [hasError, setHasError] = useState(false);
 
-  useEffect(() => {
-    const getRecommendedWines = async () => {
-      setLoading(true);
-      try {
-        const wines = await fetchRecommendWine();
-        setRecommendedList(wines);
-      } catch (error) {
-        console.error('추천 와인 목록을 가져오는 데 실패했습니다.', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getRecommendedWines();
+  const getRecommendedWines = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const wines = await fetchRecommendWine();
+      setRecommendedList(wines);
+      setHasError(false);
+    } catch (error) {
+      setHasError(true);
+      console.error('추천 와인 목록을 가져오는 데 실패했습니다.', error);
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
 
-  if (loading) {
+  useEffect(() => {
+    getRecommendedWines();
+  }, [getRecommendedWines]);
+
+  if (isloading) {
     return <RecommendWineListSkeleton count={8} />;
+  }
+
+  if (hasError) {
+    return (
+      <div className='flex h-[299px] flex-col justify-center rounded-2xl bg-gray-100 mobile:h-[241px]'>
+        <Refresh handleLoad={getRecommendedWines} iconSize='w-0 h-0' iconTextGap='gap-[10px]' buttonStyle='px-[20px] py-[8px] mobile:px-[16px] mobile:py-[6px] mobile:text-md' />
+      </div>
+    );
   }
 
   return <RecommendWine recommendedList={recommendedList} />;
